111e9c99548765a01ed895358f1f13442ae3f5aa
Karl Prieb <karl@prieb.me>
2018-08-05 19:53:36 -0300

    fix addMessage owner set algo

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,12 @@
 let messageCounter = 0
 export const setMessageOwner = counter => Math.floor(counter / 2) % 2 ? 'theirs' : 'yours'
 
-export const addMessage = text => ({
-  type: 'ADD_MESSAGE',
-  id: messageCounter++,
-  text,
-  owner: setMessageOwner(messageCounter)
-})
+export const addMessage = text => {
+  const counter = messageCounter
+  return {
+    type: 'ADD_MESSAGE',
+    id: messageCounter++,
+    text,
+    owner: setMessageOwner(counter)
+  }
+}
